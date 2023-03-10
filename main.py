13659cfa5894d0889b05520d5d7f2bcb31a426e5
effciency101 <104164504+effciency101@users.noreply.github.com>
2023-03-10 15:07:04 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+
+name = str(input("Enter ur name :"))
+surname = str(input("Enter ur second name :"))
+age = int(input("Enter ur age :"))
+email = input("Enter ur email adress :")
+password = input("Enter ur password")
+
+print(f"Ur full name is {name} {surname}.")
+print(f"And ur age is {age}")
+print(f"Ur email is: {email}")
+print(f"and ur password is {password}")
+
